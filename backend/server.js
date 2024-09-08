37bc394ec6273388480f3bf81f9bba7ad0181cd5
Om Ji <om-ji@omji.lan>
2024-09-08 23:31:52 +0530

    add middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const app  =express();
 
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
 import connectToMongoDB from './db/connectToMongoDB.js';
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/api/auth',authRoutes);
+app.use('/api/messages',messageRoutes);
 
 
 app.listen(PORT,()=>{
