138245a9838fa64e086b57f159cdc706b224887d
omji74 <omjidwivedi123@gmail.com>
2024-09-29 05:12:07 +0530

    remove unwanted

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.routes.js'
 import cors from 'cors';
 import {app,server} from './socket/socket.js'
+import path from 'path'
 
+
+const __dirname = path.resolve();
 const PORT  = process.env.PORT || 5000;
 // app.use(cors({
 //   origin: 'http://localhost:3000', // Your frontend's origin
@@ -16,12 +19,20 @@ const PORT  = process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.get('/',(req,res)=>{
-    res.send("Hii this is omi !")
-})
+// app.get('/',(req,res)=>{
+//     res.send("Hii this is omi !")
+// })
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 app.use('/api/users',userRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT,()=>{
     connectToMongoDB();
 
